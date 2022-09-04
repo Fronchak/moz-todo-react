691e6320e2c15461b743db52294d9e09cfc8f497
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-09-04 11:03:46 -0300

    Criado estrutura do formulario usando JSX

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App subject='Gabriel' />);
+root.render(<App subject='Alice' />);
